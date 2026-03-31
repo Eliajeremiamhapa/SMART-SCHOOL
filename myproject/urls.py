34146fd92cac46da5d49229af989408ded5eded9
unknown <eliamhapa34@gmail.com>
2026-03-31 14:28:11 +0300

    Fix: Redirected empty path to login in urls.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings # Muhimu kwa ajili ya settings.py
 from django.conf.urls.static import static # Muhimu kwa ajili ya picha
+from django.shortcuts import redirect # Ongeza hii
 
 urlpatterns = [
+    #  for deployment
+    path('', lambda request: redirect('auth/login/')),
+
     path('admin/', admin.site.urls),
     path('auth/', include('login.urls')),
     path('gallery/', include('gallery.urls')),
