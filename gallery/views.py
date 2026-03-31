bd9eebbf714b723c1fdcc469a2ca93e70495dd78
unknown <eliamhapa34@gmail.com>
2026-03-31 15:02:14 +0300

    Nimeondoa ulinzi wa login kwenye Gallery ili iwe wazi

--- gallery/views.py
+++ gallery/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render, get_object_or_404
 from .models import Album
 from django.contrib.auth.decorators import login_required
 
-@login_required
+# @login_required
+# tumeondoa ili tuweze ku access gallery withot lohin
 def gallery_list(request):
     # Anaona albamu za shule (Public)
     albums = Album.objects.filter(is_public=True)
     return render(request, 'gallery/list.html', {'albums': albums})
 
-@login_required
+# @login_required
 def album_detail(request, album_id):
     album = get_object_or_404(Album, id=album_id)
     # Lazy Loading: Picha zitapakiwa kadiri mtumiaji anavyozihitaji [cite: 56, 57]
